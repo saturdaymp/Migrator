52122d1edf261aca6f70a79d32a720d82a08e124
Chris C <17273852+mrbiggred@users.noreply.github.com>
2016-01-26 15:58:09 -0700

    Some code refactoring before fixing the bug. ref #11

--- Source/Migrator/Migration.cs
+++ Source/Migrator/Migration.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
-using Microsoft.SqlServer.Management.Sdk.Sfc;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
@@ -16,21 +13,18 @@ namespace Migrator
     {
 
         #region Vars
-        public const string DEFAULT_SCHEMA_MIGRATIONS_TABLE = "SchemaMigrations";
+        public const string DefaultSchemaMigrationsTable = "SchemaMigrations";
 
-        private string _migrationTable;
-        private Server _server;
+        private readonly string _migrationTable;
+        private readonly Server _server;
         #endregion
 
         #region Constructors
-        public Migration(string connectionString) : this(connectionString, DEFAULT_SCHEMA_MIGRATIONS_TABLE)
-        {
-        }
 
-        public Migration(string connectionString, string migrationTable)
+        public Migration(string connectionString, string migrationTable = DefaultSchemaMigrationsTable)
         {
             // Open the database connection.  If we can't then just let the error float up.
-            SqlConnection conn = new SqlConnection(connectionString);
+            var conn = new SqlConnection(connectionString);
             _server = new Server(new ServerConnection(conn));
 
             _migrationTable = migrationTable;
@@ -38,22 +32,11 @@ namespace Migrator
         #endregion
 
         #region Properties
-        public string MigraionTable
-        {
-            get { return _migrationTable; }
-        }
+        public string MigraionTable => _migrationTable;
+
         #endregion
 
         #region Migration
-        /// <summary>
-        /// Runs all the migrations found in the folder that haven't yet
-        /// been applied to the database.
-        /// </summary>
-        ///<param name="migrationFolder">The folder containing all the migration files.</param>
-        public void Migrate(string migrationFolder)
-        {
-            Migrate(migrationFolder, null);
-        }
 
         /// <summary>
         /// Runs all the migrations found in the folder that haven't yet
@@ -63,7 +46,7 @@ namespace Migrator
         /// <param name="statusCallback">Used to update the status of the migration (i.e. what file is being
         /// migrated, has it been migrated already, etc).</param>
         /// <exception cref="DirectoryNotFoundException">If the migration folder isn't valid.</exception>
-        public void Migrate(string migrationFolder, UpdateStatus statusCallback)
+        public void Migrate(string migrationFolder, UpdateStatus statusCallback = null)
         {
             // So we can do let the caller know how things are going.
             _updateStatusDelg = statusCallback;
@@ -73,31 +56,31 @@ namespace Migrator
             // Get all the migrations that have been ran already.
             StatusUpdate("Loading previously-applied migrations");
             var previousMigrations = GetAppliedMigrations(_migrationTable);
-            StatusUpdate(string.Format("... {0} previously-applied migrations loaded", previousMigrations.Count));
+            StatusUpdate($"... {previousMigrations.Count} previously-applied migrations loaded");
 
             // Get all the files.
-            StatusUpdate(string.Format("Loading migration files found at: {0}", migrationFolder));
-            string[] allMigrationFiles = Directory.GetFiles(migrationFolder);
-            StatusUpdate(string.Format("... {0} migration files loaded", allMigrationFiles.Length));
+            StatusUpdate($"Loading migration files found at: {migrationFolder}");
+            var allMigrationFiles = Directory.GetFiles(migrationFolder);
+            StatusUpdate($"... {allMigrationFiles.Length} migration files loaded");
 
             // Run each migration, stopping if we have any errors.
-            foreach (string migrationFile in allMigrationFiles)
+            foreach (var migrationFile in allMigrationFiles)
             {
-                StatusUpdate(string.Format("Applying migration from file: {0}", migrationFile));
+                StatusUpdate($"Applying migration from file: {migrationFile}");
 
                 // The file migration version.
-                string version = GetVersionFromFileName(migrationFile);
-                StatusUpdate(string.Format("... version: {0}", version));
+                var version = GetVersionFromFileName(migrationFile);
+                StatusUpdate($"... version: {version}");
 
                 // Has the migration been run already?
                 if (!previousMigrations.Contains(version))
                 {
                     // Run the migration.
-                    string query = File.ReadAllText(migrationFile);
+                    var query = File.ReadAllText(migrationFile);
                     _server.ConnectionContext.ExecuteNonQuery(query);
 
                     // Update the migration table but only if the script hasn't yet.
-                    int migrationTableUpdated = (int)_server.ConnectionContext.ExecuteScalar("Select Count(*) From " + _migrationTable + " Where Version = '" + version + "'");
+                    var migrationTableUpdated = (int)_server.ConnectionContext.ExecuteScalar("Select Count(*) From " + _migrationTable + " Where Version = '" + version + "'");
                     if (migrationTableUpdated == 0)
                     {
                         _server.ConnectionContext.ExecuteScalar("Insert Into " + _migrationTable + " Values(" + version + ")");
@@ -118,80 +101,15 @@ namespace Migrator
 
             StatusUpdate("Finished applying migrations");
         }
-        #endregion
 
-        #region Status Update
-        public delegate void UpdateStatus(string status);
-        private UpdateStatus _updateStatusDelg;
-
-        private void StatusUpdate(string statusUpdate)
-        {
-            if (_updateStatusDelg != null)
-            {
-                _updateStatusDelg(statusUpdate);
-            }
-        }
-        #endregion
-
-        #region Create Migration File
-        public static string CreateMigrationFile(string migrationFolder, string migrationDesc)
-        {
-            return CreateMigrationFile(migrationFolder, DEFAULT_SCHEMA_MIGRATIONS_TABLE, migrationDesc);
-        }
-
-        public static string CreateMigrationFile(string migrationFolder, string schemaTable, string migrationDesc)
-        {
-            var version = string.Format("{0:yyyyMMddHHmmss}", DateTime.Now);
-            var fileName = version + "_" + migrationDesc + ".sql";
-            var fullPath = Path.Combine(migrationFolder, fileName);
-
-            var versionUpdate = Environment.NewLine + Environment.NewLine;
-            versionUpdate += "-- Place your code before the version update." + Environment.NewLine;
-            versionUpdate += "Insert Into " + schemaTable + " Values(" + version + ")" + Environment.NewLine;
-            File.WriteAllText(fullPath, versionUpdate);
-
-            return fullPath;
-        }
-        #endregion
-
-        #region Get Applied Migrations
-        /// <summary>
-        /// Gets a list of the applied migrations using the default migration table name.
-        /// </summary>
-        /// <returns>A list of the migrations that have been applied to this database.</returns>
-        public List<string> GetAppliedMigrations()
-        {
-            return GetAppliedMigrations(DEFAULT_SCHEMA_MIGRATIONS_TABLE);
-        }
-
-        /// <summary>
-        /// Gets a list of the applied migrations.
-        /// </summary>
-        /// <param name="migrationTable">The migration table.</param>
-        /// <returns>A list of the migrations that have been applied to this database.</returns>
-        public List<string> GetAppliedMigrations(string migrationTable)
-        {
-            var ds = _server.ConnectionContext.ExecuteWithResults("Select * From " + migrationTable);
-
-            var migrations = new List<string>();
-            foreach (DataRow dr in ds.Tables[0].Rows)
-            {
-                migrations.Add(dr[0].ToString());
-            }
-
-            return migrations;
-        }
-        #endregion
-
-        #region Private Common Methods
         /// <summary>
         /// Given a migration file name will extract the version part.
         /// </summary>
         /// <param name="migrationFileName">The full migration file including the path (e.g. c:\Project Foo\Db\Migrations\20100213145632_CreateContactsTable.sql)</param>
         /// <returns>The version part of the file name.</returns>
-        private string GetVersionFromFileName(string migrationFileName)
+        private static string GetVersionFromFileName(string migrationFileName)
         {
-            return Path.GetFileName(migrationFileName.Substring(0, migrationFileName.IndexOf("_")));
+            return Path.GetFileName(migrationFileName.Substring(0, migrationFileName.IndexOf("_", StringComparison.Ordinal)));
         }
 
         /// <summary>
@@ -206,20 +124,67 @@ namespace Migrator
         private void RefreshAllViews(ServerConnection conn)
         {
             // Find all the views that are not schema bound.
-            string viewSql = @"Select Distinct name
+            const string viewSql = @"Select Distinct name
                                From sys.objects so
                                Where type = 'V'
                                And ObjectProperty(object_id, 'IsSchemaBound') = 0";
 
-            DataSet viewsDs = conn.ExecuteWithResults(viewSql);
+            var viewsDs = conn.ExecuteWithResults(viewSql);
 
 
             // Run refresh an each view.
             foreach (DataRow dr in viewsDs.Tables[0].Rows)
             {
-                string refreshSql = @"Exec sp_refreshview '" + dr["name"] + "'";
+                var refreshSql = @"Exec sp_refreshview '" + dr["name"] + "'";
                 conn.ExecuteNonQuery(refreshSql);
             }
+        }
+        #endregion
+
+        #region Status Update
+        public delegate void UpdateStatus(string status);
+        private UpdateStatus _updateStatusDelg;
+
+        private void StatusUpdate(string statusUpdate)
+        {
+            _updateStatusDelg?.Invoke(statusUpdate);
+        }
+
+        #endregion
+
+        #region Create Migration File
+        public static string CreateMigrationFile(string migrationFolder, string migrationDesc)
+        {
+            return CreateMigrationFile(migrationFolder, DefaultSchemaMigrationsTable, migrationDesc);
+        }
+
+        public static string CreateMigrationFile(string migrationFolder, string schemaTable, string migrationDesc)
+        {
+            var version = $"{DateTime.Now:yyyyMMddHHmmss}";
+            var fileName = version + "_" + migrationDesc + ".sql";
+            var fullPath = Path.Combine(migrationFolder, fileName);
+
+            var versionUpdate = Environment.NewLine + Environment.NewLine;
+            versionUpdate += "-- Place your code before the version update." + Environment.NewLine;
+            versionUpdate += "Insert Into " + schemaTable + " Values(" + version + ")" + Environment.NewLine;
+            File.WriteAllText(fullPath, versionUpdate);
+
+            return fullPath;
+        }
+        #endregion
+
+        #region Get Applied Migrations
+
+        /// <summary>
+        /// Gets a list of the applied migrations.
+        /// </summary>
+        /// <param name="migrationTable">The migration table.</param>
+        /// <returns>A list of the migrations that have been applied to this database.</returns>
+        public List<string> GetAppliedMigrations(string migrationTable = DefaultSchemaMigrationsTable)
+        {
+            var ds = _server.ConnectionContext.ExecuteWithResults("Select * From " + migrationTable);
+
+            return (from DataRow dr in ds.Tables[0].Rows select dr[0].ToString()).ToList();
         }
         #endregion
 
